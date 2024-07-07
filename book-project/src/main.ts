515c17591115ed1ebd75dce17e51f4c5e00f646d
Marco Antonio Juarez <mjuarez@adere.so>
2024-07-07 13:26:50 -0600

    Added colors and configs in the documents

--- book-project/src/main.ts
+++ book-project/src/main.ts
@@ -1,17 +1,43 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import 'vuetify/styles'
+import './assets/main.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 const vuetify = createVuetify({
-    components,
-    directives,
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#1976D2',
+          secondary: '#424242',
+          accent: '#82B1FF',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FB8C00',
+        },
+      },
+      dark: {
+        colors: {
+          primary: '#BB86FC',
+          secondary: '#03DAC6',
+          accent: '#03DAC6',
+          error: '#CF6679',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FB8C00',
+        },
+      },
+    },
+  },
 })
 
-createApp(App).use(vuetify).use(router).mount('#app')
+
+createApp(App).use(router).use(vuetify).mount('#app')
---
+++ book-project/src/views/BPHome.vue
@@ -0,0 +1,7 @@
+<template>
+    <BPLogin />
+</template>
+
+<script lang="ts" setup>
+import BPLogin from '@/views/BPLogin.vue'
+</script>
