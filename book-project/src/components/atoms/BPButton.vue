7354aec463271534d38b26ee7b678ff28e5b1b2d
Marco Antonio Juarez <mjuarez@adere.so>
2024-07-08 16:34:20 -0600

    Added component for button

---
+++ book-project/src/components/atoms/BPButton.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-btn
+        :color="color"
+        :variant="variant" 
+        :size="size"
+        :prepend-icon="prependIcon" 
+        :append-icon="appendIcon"
+    >
+        <slot>
+            <v-icon v-if="icon">{{ icon }}</v-icon>
+        </slot>
+    </v-btn>
+  </template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    color: {
+        type: String,
+        default: 'base'
+    },
+    variant: {
+        type: String,
+        default: 'flat'
+    },
+    size: {
+        type: String,
+        default: 'default'
+    },
+    icon: {
+      type: String,
+      required: false,
+    },
+    prependIcon: {
+        type: String,
+        required: false
+    },
+    appendIcon: {
+        type: String,
+        required: false
+    },
+  });
+
+</script>
