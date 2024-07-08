b61abbcec1a2fc54987df61bf4fb658bdf76b990
Marco Antonio Juarez <mjuarez@adere.so>
2024-07-08 16:13:32 -0600

    Added dark-mode Toggle

--- book-project/src/views/BPLogin.vue
+++ book-project/src/views/BPLogin.vue
@@ -1,9 +1,10 @@
 <template>
     <h1>Aqui va el logiiiiiiiiiiin</h1>
+    <BPThemeToggle />
 </template>
 
 <script setup lang="ts">
-
+import BPThemeToggle from '@/components/atoms/BPThemeToggle.vue';
 </script>
 
 <style scoped>
