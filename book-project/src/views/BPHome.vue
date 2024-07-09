3f3eccbaa9f186caec35789fc5fbfae0822d7f1f
Marco Antonio Juarez <mjuarez@adere.so>
2024-07-09 16:09:36 -0600

    Added sign up view and multiple reusable components

--- book-project/src/views/BPHome.vue
+++ book-project/src/views/BPHome.vue
@@ -1,20 +1,7 @@
 <template>
-    <div class="home">
-        <BPSignup />
-    </div>
+    <BPLogin />
 </template>
 
-<script setup>
-import BPSignup from '@/views/BPSignup.vue';
+<script lang="ts" setup>
+import BPLogin from '@/views/BPLogin.vue'
 </script>
-
-<style scoped>
-.home {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    width: 100vw;
-    background-color: var(--background-secondary);
-}
-</style>
