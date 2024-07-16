906df23cad1dcaa9b64bcc943f00c3a54a669adb
Marco Antonio Juarez <mjuarez@adere.so>
2024-07-16 14:10:32 -0600

    Added side menu

--- book-project/src/views/BPHome.vue
+++ book-project/src/views/BPHome.vue
@@ -1,7 +1,9 @@
 <template>
     <BPLogin />
+    <BPSideMenu />
 </template>
 
 <script lang="ts" setup>
 import BPLogin from '@/views/BPLogin.vue'
+import BPSideMenu from '@/components/organisms/BPSideMenu.vue';
 </script>
