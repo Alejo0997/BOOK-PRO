f9df408c40ec2a01ebf54a3eb14e62aa7de004f8
Marco Antonio Juarez <mjuarez@adere.so>
2024-07-19 14:24:55 -0600

    Added styles for home

--- book-project/src/views/BPUserProfile.vue
+++ book-project/src/views/BPUserProfile.vue
@@ -1,234 +1,63 @@
 <template>
+    <BPSideMenu />
     <div class="bp-user-profile d-flex flex-column align-center ga-2 pa-2">
-    
-        <div class="title-container">
-    
-            <h1>Mi Perfil</h1>
-    
+        <div class="user h-25 d-flex ga-5">
+            <v-card class="w-50">
+                wrefwerf
+            </v-card>
+            <v-card class="w-50">
+                wrefwrf
+            </v-card>
         </div>
-    
-    
-    
-        <div class="profile-section d-flex flex-column">
-    
-            <img class="cover-photo" :src="coverPhoto" alt="Cover Photo" />
-    
-            <div class="profile-header">
-    
-                <div class="profile-info">
-    
-                    <div class="profile-container-photo">
-    
-                        <img class="profile-photo" :src="profilePhoto" alt="Profile Photo" />
-    
-                    </div>
-    
-    
-    
-                    <BPButton @click="editProfile" class="edit-button">Editar Perfil</BPButton>
-    
-                </div>
-    
-            </div>
-    
-        </div>
-    
-        <div class="books-section">
-    
-            <div class="content-buttons">
-    
+        <v-card class="books-section pa-5 mt-3">
+            <div class="d-flex justify-center mb-5">
                 <BPButton @click="showReadBooks" :class="{ active: view === 'read' }">Leídos</BPButton>
-    
                 <BPButton @click="showInterestedBooks" :class="{ active: view === 'interested' }">
-    
                     Me Interesa
-    
                 </BPButton>
-    
             </div>
-    
-            <div class="book-views">
-    
-                <div v-if="view === 'read'">
-    
-                    <h2>Libros Leídos</h2>
-    
-                    <ul>
-    
-                        <li v-for="book in readBooks" :key="book.id">{{ book.title }}</li>
-    
-                    </ul>
-    
-                </div>
-    
-                <div v-if="view === 'interested'">
-    
-                    <h2>Libros que Me Interesan</h2>
-    
-                    <ul>
-    
-                        <li v-for="book in interestedBooks" :key="book.id">{{ book.title }}</li>
-    
-                    </ul>
-    
-                </div>
-    
-            </div>
-    
-        </div>
-    
+            <BPIterator />
+        </v-card>
     </div>
 </template>
 
-<script>
-export default {
-    name: 'BPUserProfile',
-    data() {
-        return {
-            view: 'read',
-            profilePhoto: 'path/to/profile/photo.jpg',
-            coverPhoto: 'path/to/cover/photo.jpg',
-            readBooks: [
-                { id: 1, title: 'Libro Leído 1' },
-                { id: 2, title: 'Libro Leído 2' }
-                // más libros leídos
-            ],
-            interestedBooks: [
-                { id: 1, title: 'Libro Interesante 1' },
-                { id: 2, title: 'Libro Interesante 2' }
-                // más libros interesantes
-            ]
-        }
-    },
-    methods: {
-        editProfile() {
-            // Lógica para editar el perfil
-            console.log('Editar Perfil')
-        },
-        showReadBooks() {
-            this.view = 'read'
-        },
-        showInterestedBooks() {
-            this.view = 'interested'
-        }
-    }
+<script setup>
+import { ref } from 'vue';
+import BPButton from '@/components/atoms/BPButton.vue';
+import BPIterator from '@/components/atoms/BPIterator.vue';
+import BPSideMenu from '@/components/organisms/BPSideMenu.vue';
+
+const view = ref('read');
+
+function showReadBooks() {
+    view.value = 'read';
 }
-import BPButton from '@/components/atoms/BPButton.vue'
+
+function showInterestedBooks() {
+    view.value = 'interested';
+}
 </script>
 
 <style scoped>
 .bp-user-profile {
     width: 100vw;
     height: 100vh;
-    background-color: #28272a;
-}
-
-.title-container {
-    width: 90%;
-    text-align: start;
-    color: white;
-}
-
-.profile-section {
-    background-color: #42b983;
-    height: 100%;
-    width: 90%;
-    border-radius: 20px;
-    overflow: hidden;
-}
-
-.profile-header {
-    text-align: center;
-}
-
-.cover-photo {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-}
-
-.profile-info {
-    background-color: #434146;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    height: 8em;
-    padding: 0 6em 0 0px;
-}
-
-.profile-photo {
-    width: 200px;
-    height: 200px;
-    border-radius: 50%;
-    border: 5px solid white;
-    position: absolute;
-    bottom: 40px;
-}
-
-.profile-container-photo {
-    width: 200px;
-    height: 200px;
-    position: relative;
-}
-
-.edit-button {
-    margin-left: 20px;
-    padding: 10px 20px;
-    background-color: #2c89f9;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
+    background-color: var(--background-secondary);
 }
 
 .books-section {
     height: 100%;
-    width: 90%;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    padding: 20px;
-    background-color: #434146;
-    border-radius: 20px;
+    width: 80%;
     overflow: hidden;
-    color: white;
 }
 
-.content-buttons {
-    display: flex;
-    justify-content: center;
-    margin-bottom: 20px;
-}
-
-.content-buttons BPButton {
-    padding: 10px 20px;
-    margin: 0 10px;
-    border: 1px solid black;
-    background-color: transparent;
-    cursor: pointer;
-}
-
-.content-buttons .active {
+.active {
     background-color: #2c89f9;
     color: white;
 }
 
-.book-views {
-    overflow-y: auto;
-    flex: 1;
+.user {
+    width: 80%;
 }
 
-.book-views h2 {
-    text-align: center;
-}
-
-.book-views ul {
-    list-style: none;
-    padding: 0;
-}
-
-.book-views li {
-    margin: 5px 0;
-}
 </style>
